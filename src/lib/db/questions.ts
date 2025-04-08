3aaf6b768d07deb342c179641d895909772412c7
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-04-08 12:30:31 +0530

    fix: get_questions() function, fixed the filter bug

--- src/lib/db/questions.ts
+++ src/lib/db/questions.ts
@@ -1,6 +1,6 @@
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
-import { Question } from "@/types/question"
+import { Question, Filter } from "@/types/question"
 
 const COLLECTION_NAME = "questions";
 
@@ -8,12 +8,14 @@ export async function add_questions(question_data: Question) {
   const client = await clientPromise;
   const db = client.db();
   const result = await db.collection(COLLECTION_NAME).insertOne(question_data);
+
   return result.insertedId;
 }
 
 export async function update_question(id: string, updated_data: Question) {
   const client = await clientPromise;
   const db = client.db();
+
   return await db.collection(COLLECTION_NAME).updateOne(
     { _id: new ObjectId(id) },
     { $set: updated_data}
@@ -23,10 +25,10 @@ export async function update_question(id: string, updated_data: Question) {
 export async function delete_question(id: string) {
   const client = await clientPromise;
   const db = client.db();
+
   return await db.collection(COLLECTION_NAME).deleteOne(
     { _id: new ObjectId(id) }
   )
-
 }
 
 export async function get_question_by_id(id: string) {
@@ -37,13 +39,18 @@ export async function get_question_by_id(id: string) {
   )
 
   return result;
-
 }
 
-export async function get_questions(filter = {}) {
+export async function get_questions(filter: Filter) {
   const client = await clientPromise;
   const db = client.db();
-  const result = db.collection(COLLECTION_NAME).find(filter)
+  const result = db.collection(COLLECTION_NAME).find(
+    {
+      topic: filter.topic,
+      catagory: filter.catagory,
+      difficulty: filter.difficulty,
+    }
+  )
 
   return result;
 }
