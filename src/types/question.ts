88e405c6eb52736e7038bac9bb3155590a6c2992
sammy144 <samreeves1404@gmail.com>
2025-05-22 09:28:44 +0530

    question type configured

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,16 +1,21 @@
 import { ObjectId } from "mongodb";
-export type QuestionOptionKey = "option_1" | "option_2" | "option_3" | "option_4" | "option_5";
-export type QuestionType = "mcq" | "coding";
-export type OptionType = "multiple" | "single";
 
-export interface Question {
+type QuestionOptionKey = "option_1" | "option_2" | "option_3" | "option_4" | "option_5";
+type QuestionType = "mcq" | "coding";
+type OptionType = "multiple" | "single";
+
+type BaseQuestion = {
   category: string;
   topic: string;
   difficulty: string;
   text: string; 
   image?: string;
-  questionType: QuestionType,
   marks: string; 
+}
+
+
+type MCQ = BaseQuestion & {
+  questionType: "mcq";
   optionType: OptionType;
   options: {
     option_1: string;
@@ -22,9 +27,31 @@ export interface Question {
   answer: QuestionOptionKey; 
 }
 
+type Coding = BaseQuestion & {
+  questionType: "coding";
+  sampleResults: sampleResults[];
+  hiddenTestCases: hiddenTestCases[];
+  solution?: string
+}
+
+type sampleResults = {
+  input: string;
+  output: string;
+}
+
+
+type hiddenTestCases = {
+  input: string;
+  output: string;
+}
+
+
 export interface Filter {
   _id?: ObjectId;
   topic?: string;
   category?: string;
   difficulty?: string;
 }
+
+
+export type Question = MCQ | Coding;
